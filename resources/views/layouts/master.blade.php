f809f645042b4a36d09ba0521b39527c67a92816
amine.lamchatab <115371227+LamchatabAmine@users.noreply.github.com>
2024-08-05 16:17:54 +0100

    update script build js

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -13,13 +13,13 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-  
+
     @env('local')
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     @endenv
     @production
         <link rel="stylesheet" href="{{ asset('build/assets/app-56d27ad2.css') }}">
-        <script src="{{ asset('build/assets/app-33903afe.js') }}"></script>
+        <script src="{{ asset('build/assets/app-app-6c268de4.js') }}"></script>
     @endproduction
 
     @stack('third_party_stylesheets')
