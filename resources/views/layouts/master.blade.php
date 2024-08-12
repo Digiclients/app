0ae1298cb090cd49ad01844ff627d18cf89b41e4
Your Name <you@example.com>
2024-08-12 19:42:48 +0100

    google analitics

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -26,6 +26,17 @@
     @stack('page_css')
 </head>
 
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-8FR2M9TYKH"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-8FR2M9TYKH');
+</script>
+
+
 <body>
 
     @yield('_content')
