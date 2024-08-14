e0adfb94acff1227eb1dd0669319f67ea69367d5
Your Name <you@example.com>
2024-08-14 05:02:18 +0100

    ...

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,7 +1,10 @@
 @extends('layouts.auth')
 
 @section('content')
-    <div class="container">
+
+<div class="container">
+    <br><br><br><br>
+    <br><br><br><br>
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
