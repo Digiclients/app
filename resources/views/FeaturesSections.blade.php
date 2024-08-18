f0cf04305d92944e0092e6252f0e6168be38229b
Your Name <you@example.com>
2024-08-18 13:33:36 +0100

    ...

---
+++ resources/views/FeaturesSections.blade.php
@@ -0,0 +1,36 @@
+{{-- @dd($error) --}}
+@extends('layouts.app')
+@push('third_party_stylesheets')
+@endpush
+
+@section('content')
+ 
+
+
+<br><br><br><br><br>
+
+    <section id="PrixMoyen" class="AverageSearch px-4 px-xl-0 py-5 container-fluid row justify-content-center">
+        <div class="col-lg-12 col-xl-11 col-xxl-9 bgLight rounded  p-4">
+
+            <div class="TheHeading text-center py-4">
+
+                <h1 class="darkcolor fontwbold font30">Entrez les détails de votre véhicule et découvrez instantanément sa valeur réelle sur le marché de l'occasion</h1>
+                {{-- <p class="darkcolor font18 fontw600 m-0">Une analyse des Moyennes de </p>
+                <p class="primarycolor font30 fontw900 m-0">842 985</p>
+                <p class="darkcolor font18 fontw600 m-0">prix d'annonces à travers la France.</p> --}}
+            </div>
+
+
+
+                <div class="bgwhite my-5 py-5 rounded" id="averageStatistics">
+                  hfghfghfghfhf <br><br><br><br>
+                </div>
+        </div>
+    </section>
+
+
+
+
+@push('third_party_scripts')
+   
+@endpush
